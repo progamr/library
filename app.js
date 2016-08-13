c4c1aa9c077f2c84f86bca449700970645d20c74
progamr <prog.amr2014@gmail.com>
2016-08-13 11:57:44 +0200

    start building routes

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 var express = require('express');
 var app = express();
 
-var port = 5000;
-
-app.get('/', function (req, res) {
-    res.send('hello world');
+var port = process.env.PORT || 5000;
+app.use(express.static('public'));
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+app.get('/', function(req, res) {
+    res.render('index', {
+        title: 'hello from ejs',
+        nav: [
+            {
+                link: '/books',
+                text: 'Books',
+            },
+            {
+                link: '/authors',
+                text: 'Authors',
+            },
+        ],});
 });
-app.get('/books', function (req, res) {
+app.get('/books', function(req, res) {
     res.send('hello books');
 });
 
-app.listen(port, function (err) {
-   console.log('running server on port ' + port);
+app.listen(port, function(err) {
+    console.log('running server on port ' + port);
 });
